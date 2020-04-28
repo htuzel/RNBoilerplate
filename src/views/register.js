a9f1be134630362792c5838d542506f111f13fe3
Hayreddin Tüzel <hayreddintuzel@gmail.com>
2020-04-28 19:31:35 +0300

    register screen

--- src/views/register.js
+++ src/views/register.js
@@ -1,20 +1,91 @@
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
 import BoxCenter from '../components/boxcenter';
 import {Logo} from '../components/icons';
-import styled from 'styled-components/native';
 import {inject, observer} from 'mobx-react';
+import {Input, Card, Button} from '@ui-kitten/components';
+import {store} from '../store';
 
 @inject('store')
 @observer
 class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    let {navigation} = props;
+    this.state = {
+      email: '',
+      password: '',
+      name: '',
+    };
+  }
+
+  emailHandleChange = text => {
+    this.setState({email: text});
+  };
+
+  passwordHandleChange = text => {
+    this.setState({password: text});
+  };
+
+  nameHandleChange = text => {
+    this.setState({name: text});
+  };
+
+  submitForm = async () => {
+    try {
+      let response = await fetch('https://turacoon.com/api/register', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.password,
+          name: this.state.name,
+        }),
+      });
+      let user = await response.json();
+      if (user.data.api_token) {
+        this.props.store.setAuthUser();
+        this.props.store.setApiToken(user.data.api_token);
+      }
+      console.warn(this.props.store.isAuthenticated);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   render() {
     const {navigation} = this.props;
     const isAuthenticated = this.props.store.isAuthenticated;
     return (
-      <BoxCenter>
-        <Logo />
-        <Text>Register</Text>
+      <BoxCenter behavior="padding">
+        <Card>
+          <Logo />
+          <Input
+            autoCapitalize="none"
+            label="Your Name"
+            value={this.state.name}
+            onChangeText={this.nameHandleChange}
+          />
+          <Input
+            autoCapitalize="none"
+            label="Your Email"
+            value={this.state.email}
+            onChangeText={this.emailHandleChange}
+          />
+          <Input
+            autoCapitalize="none"
+            label="Password"
+            caption="Should contain at least 8 symbols"
+            secureTextEntry={true}
+            onChangeText={this.passwordHandleChange}
+            value={this.state.password}
+          />
+          <Button onPress={this.submitForm} style={{marginTop: 15}}>
+            Register
+          </Button>
+        </Card>
       </BoxCenter>
     );
   }
